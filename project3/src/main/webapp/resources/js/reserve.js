43a5cc2cca1cbae0ceda354c04616ec658cc6499
yonghyun123 <model9960@naver.com>
2020-11-29 22:07:55 +0900

    descript opening hours and place about product

---
+++ project3/src/main/webapp/resources/js/reserve.js
@@ -0,0 +1,17 @@
+// 첫 화면 ajax
+// DOM init function
+document.addEventListener('DOMContentLoaded', function() {
+	init();
+})
+
+function init() {
+//	var productId = document.querySelector("#productId").value;
+//	var imageTypeUrl = "/project3/products/detail/" + productId;
+//	var reviewUrl = "/project3/products/detail/review/" + productId;
+//	
+//	detailImageAjax(imageTypeUrl);
+//	openClose(); //접기 펼치기 기능
+//	DetailTab.registerEvents(); //객체리터럴을 이용한 TabUI 구현 
+//	reviewAjax(reviewUrl); //detail 페이지에서 리뷰를 가져오기 위한 ajax
+	
+}
